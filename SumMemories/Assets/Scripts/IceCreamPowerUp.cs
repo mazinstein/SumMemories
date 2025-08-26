7cfe5bdd7efd0f492eea49a87cc7308084cbe968
Aleksandr <mazuninalex1961@gmail.com>
2025-08-26 09:20:07 +0300

    PowerUp

---
+++ SumMemories/Assets/Scripts/IceCreamPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class IceCreamPowerUp : MonoBehaviour
+{
+    public float healAmount = 20f; // сколько хп восстанавливаем
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.Heal(healAmount);
+            }
+
+            Destroy(gameObject); // уничтожаем мороженое
+        }
+    }
+}
